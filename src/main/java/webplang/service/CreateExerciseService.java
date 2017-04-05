d68c027bc27c6bba2c6ffbbbfc6797e1cf3272d1
WoKak <michal.woj34@gmail.com>
2017-04-05 21:01:06 +0200

    Creating exercise works (CreateExerciseService class, ApplicationController changes)

---
+++ src/main/java/webplang/service/CreateExerciseService.java
@@ -0,0 +1,77 @@
+package webplang.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import webplang.domain.Exercise;
+import webplang.domain.Word;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Michał on 2017-04-05.
+ */
+
+@Service
+public class CreateExerciseService {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public CreateExerciseService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void initializeExercise(Exercise exercise) {
+
+        ArrayList<Integer> alreadyInExercise = new ArrayList<Integer>(0);
+        Random random = new Random();
+        String query = "SELECT * FROM words WHERE id = CAST(? AS integer)";
+        Integer size = 0;
+        int counter = 0;
+
+
+        try {
+
+            Connection conn = dataSource.getConnection();
+
+            Statement stat = conn.createStatement();
+
+            ResultSet result = stat.executeQuery("SELECT * FROM words");
+
+            while (result.next()) {
+                size = Integer.parseInt(result.getString(1));
+            }
+
+            size++;
+
+            while (counter < 20) {
+
+                Integer idx = random.nextInt(size);
+
+                while (alreadyInExercise.contains(idx)) {
+                    idx = random.nextInt(size);
+                }
+
+                PreparedStatement pstat = conn.prepareStatement(query);
+                pstat.setString(1, String.valueOf(idx));
+
+                result = pstat.executeQuery();
+
+                if (result.next()) {
+
+                    Word tmp = new Word(result.getString(2).trim(), result.getString(3).trim());
+                    exercise.getWords().add(tmp);
+                    counter++;
+                }
+
+                alreadyInExercise.add(idx);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- src/main/java/webplang/web/ApplicationController.java
+++ src/main/java/webplang/web/ApplicationController.java
@@ -1,10 +1,14 @@
 package webplang.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import webplang.domain.Answer;
+import webplang.domain.Exercise;
+import webplang.domain.Word;
+import webplang.service.CreateExerciseService;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -17,6 +21,15 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping("/application")
 public class ApplicationController {
 
+    private CreateExerciseService ces;
+    private Exercise exercise;
+
+    @Autowired
+    public ApplicationController(CreateExerciseService ces) {
+        this.ces = ces;
+        this.exercise = new Exercise();
+    }
+
     @RequestMapping(method = GET)
     public String getWordInEnglishFromApplicationForm(Model model) {
 
@@ -28,7 +41,11 @@ public class ApplicationController {
     @RequestMapping(method = POST)
     public String processApplicationForm(@ModelAttribute("userAnswer") Answer userAnswer) {
 
-        System.out.println(userAnswer.toString());
+        this.ces.initializeExercise(this.exercise);
+        for (Word w: this.exercise.getWords()) {
+
+            System.out.println(w.toString());
+        }
         return "application";
     }
 
